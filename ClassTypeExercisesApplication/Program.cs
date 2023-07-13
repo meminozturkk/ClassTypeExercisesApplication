942e8cd9fe8cf6aa9a534729bbb8d057630a2f1c
meminozturkk <180101051m@gmail.com>
2023-07-13 14:07:30 +0300

    Add project files.

---
+++ ClassTypeExercisesApplication/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utilities;
+
+namespace ClassTypeExercisesApplication
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			UseCurrencyFormatter();
+			Console.ReadLine();
+		}
+
+		private static void UseCurrencyFormatter()
+		{
+			List<decimal> values = new List<decimal>{2574.8m,32423,423423};
+
+			foreach (var value in values)
+			{
+
+				string formattedValue = value.DisplayAsCurrency();
+
+				Console.WriteLine(formattedValue);
+            }
+		}
+	}
+}
---
+++ Utilities/CurrencyFormatUtility.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities
+{
+	public static class CurrencyFormatUtility
+	{
+		public static string DisplayAsCurrency(this decimal value)
+		{
+			return string.Format("{0:C} TL",value);
+		}
+	}
+}
